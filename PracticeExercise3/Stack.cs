a25b6cb501a2529cbc7c1435580a312c96a6140e
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2022-10-21 15:12:55 -0500

    In Class Progress

---
+++ PracticeExercise3/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercise3
+{
+    public class Stack<T> : IStack<T>
+    {
+        private LinkedList<T> linkedList;
+        public Stack()
+        {
+            linkedList = new LinkedList<T>();
+        }
+        public bool IsEmpty => linkedList.Count == 0;
+
+        public int Length => linkedList.Count;
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new EmptyStackException();
+            }
+            return linkedList.Last.Value;
+        }
+
+        public T Pop()
+        {
+            if(IsEmpty)
+            {
+                throw new EmptyStackException();
+            }
+
+            var top = linkedList.Last.Value; //.Value will get the node's data
+            linkedList.RemoveLast();
+
+            return top;
+        }
+
+        public void Push(T item)
+        {
+            linkedList.AddLast(item);
+        }
+    }
+}
